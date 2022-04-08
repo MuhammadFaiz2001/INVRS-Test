d225aec94888b0dbe04040a47de4a93e9c9a0e6f
Muhammad Faiz <faiz.muhammad1002@gmail.com>
2022-04-08 17:51:56 -0400

    first commit

---
+++ test.js
@@ -0,0 +1,42 @@
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const chromedriver = require('chromedriver');
+
+chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
+
+ 
+var driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .setChromeOptions( /* … */)
+    .build();
+
+  
+
+    describe( 'Test Suite' , function(){
+      this.timeout(15000);
+      before(function(){
+   
+          driver.get( "https://test.invrs.com/home" );
+          driver.get( "https://test-app.invrs.com/index.html#/login" );
+          driver.findElement(webdriver.By.id("username")).sendKeys("faiz.muhammad1002@gmail.com");
+          driver.findElement(webdriver.By.id("password")).sendKeys("testing1");
+          // a promise is returned while ‘click’ action
+          // is registered in ‘driver’ object
+          
+          
+           return driver.findElement(webdriver.By.className("p-button p-component sign-in-button")).click();
+      });
+   
+      after(function(){
+   
+          return driver.quit();
+   
+      });
+   
+      it( 'Test Case', function(){
+        
+        
+         var condition = webdriver.until.elementLocated(webdriver.By.className('p-autocomplete-input p-inputtext p-component'));
+         return driver.wait(condition, 10000);
+      
+        })});
